f3a39f44fe108e52ae9e4a55cd8f0f51a4f3ca53
dee <dee@192.168.1.107>
2015-06-06 00:49:23 +0800

    add style

--- src/com/yongchun/sildingupdwon/MainActivity.java
+++ src/com/yongchun/sildingupdwon/MainActivity.java
@@ -1,26 +1,52 @@
 package com.yongchun.sildingupdwon;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+@SuppressLint("NewApi")
 public class MainActivity extends ActionBarActivity {
-	private SildingView dragView;
+	public SildingView dragView;
+	private ListView base_list;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 		dragView = (SildingView) findViewById(R.id.dragView);
+		base_list = (ListView)findViewById(R.id.base_list);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+		adapter.addAll(getResources().getStringArray(R.array.countries));
+		base_list.setAdapter(adapter);
+		base_list.setOnScrollListener(new OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				
+			}
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				if (base_list != null && base_list.getChildCount() > 0) {
+		            if (base_list.getChildAt(0).getTop() < 0) {
+		            	dragView.setDragStart(false);
+		                return;
+		            }
+		        }
+				dragView.setDragStart(true);
+			}
+		});
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
@@ -31,7 +57,7 @@ public class MainActivity extends ActionBarActivity {
 		}else if(id == R.id.cover_enable){
 			dragView.setCover(!dragView.isCover());
 		}else if(id == R.id.top_position){
-			dragView.setTopPosition(dragView.getTopPosition()==0?200:0);
+			dragView.setTopOffset(dragView.getTopOffset()==0?200:0);
 		}
 		return super.onOptionsItemSelected(item);
 	}
--- src/com/yongchun/sildingupdwon/SildingView.java
+++ src/com/yongchun/sildingupdwon/SildingView.java
@@ -3,15 +3,18 @@ package com.yongchun.sildingupdwon;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Handler;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-public class SildingView extends RelativeLayout {
+public class SildingView extends FrameLayout {
 
 	private View mHeaderView;
 	private View mContentView;
@@ -19,7 +22,7 @@ public class SildingView extends RelativeLayout {
 	private int headerViewHeight = 0;
 	private int contentViewHeight = 0;
 	// 上拉边界,在距离顶部多少位置停止上拉
-	private int topPosition = 0;
+	private int topOffset = 0;
 	// 是否可以下拉
 	private boolean isPullEnable = true;
 	// 是否可以上拉
@@ -29,13 +32,22 @@ public class SildingView extends RelativeLayout {
 
 	private OnDragListener listener;
 
-	private ViewDragHelper dragHelper = ViewDragHelper.create(this,
-			new DragCallBack());
-
+	private ViewDragHelper dragHelper;
+	// 可移动距离
+	private int dragRange;
 	// 当前content 移动了多少距离(距离top位置)
 	private int contentViewtop = 0;
 	// 阻尼系数
-	private static final float FRICTION = 1.5f;
+	private static final float FRICTION = 2f;
+
+	private int mTouchSlop = 0;
+
+	private boolean isDragStart = true;
+
+	// Used for scrolling
+	private boolean dispatchingChildrenDownFaked = false;
+	private boolean dispatchingChildrenContentView = false;
+	private float dispatchingChildrenStartedAtY = Float.MAX_VALUE;
 
 	public interface OnDragListener {
 		void onPull();
@@ -60,61 +72,86 @@ public class SildingView extends RelativeLayout {
 
 		TypedArray ta = context.obtainStyledAttributes(attrs,
 				R.styleable.SildingView);
-		int headerView = ta.getResourceId(R.styleable.SildingView_headerView, -1);
-		if (headerView != -1) {
-			setHeaderView(headerView);
-		}
-		int contentView = ta
-				.getResourceId(R.styleable.SildingView_contentView, -1);
-		if (contentView != -1) {
-			setContentView(contentView);
-		}
-		int position = ta.getInt(R.styleable.SildingView_topPosition, 0);
-		setTopPosition(position);
-		boolean isPull = ta.getBoolean(R.styleable.SildingView_pullEnable, true);
+		// int headerView = ta.getResourceId(R.styleable.SildingView_headerView,
+		// -1);
+		// if (headerView != -1) {
+		// setHeaderView(headerView);
+		// }
+		// int contentView =
+		// ta.getResourceId(R.styleable.SildingView_contentView,
+		// -1);
+		// if (contentView != -1) {
+		// setContentView(contentView);
+		// }
+		int position = ta.getInt(R.styleable.SildingView_topOffset, 0);
+		setTopOffset(position);
+		boolean isPull = ta
+				.getBoolean(R.styleable.SildingView_pullEnable, true);
 		setPullEnable(isPull);
-		boolean isPush = ta.getBoolean(R.styleable.SildingView_pushEnable, true);
+		boolean isPush = ta
+				.getBoolean(R.styleable.SildingView_pushEnable, true);
 		setPushEnable(isPush);
 		boolean isCover = ta.getBoolean(R.styleable.SildingView_isCover, true);
 		setCover(isCover);
 		ta.recycle();
+
+		dragHelper = ViewDragHelper.create(this, 1.0f, new DragCallBack());
+		ViewConfiguration config = ViewConfiguration.get(context);
+		mTouchSlop = config.getScaledTouchSlop();
+	}
+
+	@Override
+	public void onFinishInflate() {
+		super.onFinishInflate();
+
+		if (getChildCount() < 2) {
+			throw new RuntimeException(
+					"Content view must contains two child views at least.");
+		}
+		mHeaderView = getChildAt(0);
+		mContentView = getChildAt(1);
 	}
 
 	private class DragCallBack extends ViewDragHelper.Callback {
 		@Override
 		public boolean tryCaptureView(View view, int arg1) {
 			// 可以移动的View
-			return view.equals(mContentView);
+			return view == mContentView;
 		}
 
 		@Override
 		public void onViewReleased(View releasedChild, float xvel, float yvel) {
-			// 松开后回到的位置
-			int finalTop;
-			// 下拉，且位置在header下面
-			if (contentViewtop >= headerViewHeight) {
-				finalTop = headerViewHeight;
-			} else {
-				// 位置在上面判断手势，下拉回到初始位置、上拉(isPushEnable ? topPosition :
-				// headerViewHeight)
-				finalTop = yvel > 0 ? headerViewHeight : topPosition;
+			// 移动大于有效距离
+			if (Math.abs(yvel) < mTouchSlop
+					&& contentViewtop < headerViewHeight) {
+				return;
 			}
-			// dragHelper.settleCapturedViewAt(releasedChild.getLeft(), top);
-			dragHelper.smoothSlideViewTo(releasedChild,
-					releasedChild.getLeft(), finalTop);
+			// 手势后续操作
+			int finalTop = yvel > 0 || contentViewtop >= headerViewHeight ? headerViewHeight
+					: topOffset;
+			dragHelper.settleCapturedViewAt(releasedChild.getLeft(), finalTop);
 			postInvalidate();
+		}
+
+		@Override
+		public int getViewVerticalDragRange(View child) {
+			return dragRange;
 		}
 
 		@Override
 		public void onViewPositionChanged(View changedView, int left, int top,
 				int dx, int dy) {
+			super.onViewPositionChanged(changedView, left, top, dx, dy);
 			contentViewtop = top;
 			requestLayout();
+			if (dispatchingChildrenContentView) {
+				resetDispatchingContentView();
+			}
 		}
 
 		@Override
 		public int clampViewPositionVertical(View child, int top, int dy) {
-			int maxTop = isPushEnable ? topPosition : headerViewHeight;
+			int maxTop = isPushEnable ? topOffset : headerViewHeight;
 			int maxBottom = isPullEnable ? getHeight() : headerViewHeight;
 			// 下拉阻尼效果
 			int newTop = top > headerViewHeight ? (int) (contentViewtop + dy
@@ -125,17 +162,6 @@ public class SildingView extends RelativeLayout {
 	}
 
 	@Override
-	public boolean onInterceptTouchEvent(MotionEvent ev) {
-		return dragHelper.shouldInterceptTouchEvent(ev);
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		dragHelper.processTouchEvent(ev);
-		return true;
-	}
-
-	@Override
 	public void computeScroll() {
 		if (dragHelper.continueSettling(true)) {
 			ViewCompat.postInvalidateOnAnimation(this);
@@ -143,25 +169,78 @@ public class SildingView extends RelativeLayout {
 	}
 
 	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		try {
+			boolean intercept = isDragStart
+					&& dragHelper.shouldInterceptTouchEvent(ev);
+			return intercept;
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		final int action = MotionEventCompat.getActionMasked(event);
+		if (!dispatchingChildrenContentView) {
+			try {
+				dragHelper.processTouchEvent(event);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (action == MotionEvent.ACTION_MOVE && contentViewtop == 0) {
+			dispatchingChildrenContentView = true;
+			if (!dispatchingChildrenDownFaked) {
+				dispatchingChildrenStartedAtY = event.getY();
+				event.setAction(MotionEvent.ACTION_DOWN);
+				dispatchingChildrenDownFaked = true;
+			}
+			mContentView.dispatchTouchEvent(event);
+		}
+
+		if (dispatchingChildrenContentView
+				&& dispatchingChildrenStartedAtY < event.getY()) {
+			resetDispatchingContentView();
+		}
+
+		if (action == MotionEvent.ACTION_UP
+				|| action == MotionEvent.ACTION_CANCEL) {
+			resetDispatchingContentView();
+			mContentView.dispatchTouchEvent(event);
+		}
+
+		return true;
+	}
+
+	private void resetDispatchingContentView() {
+		dispatchingChildrenDownFaked = false;
+		dispatchingChildrenContentView = false;
+		dispatchingChildrenStartedAtY = Float.MAX_VALUE;
+	}
+
+	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		super.onLayout(changed, l, t, r, b);
+		dragRange = getHeight();
 		// 获取header、content的高度
 		if (headerViewHeight == 0 || contentViewHeight == 0) {
 			contentViewtop = headerViewHeight = mHeaderView.getHeight();
 			contentViewHeight = mContentView.getHeight();
 		}
 		final int contentTopTemp = contentViewtop;
-		resetHeaderHeight(contentTopTemp);
-		resetContentHeight(contentTopTemp);
 		// header是平移模式，则设置top
-		int headerTop = !isCover && contentTopTemp <= headerViewHeight ? contentTopTemp
-				- headerViewHeight
-				: t;
+		int headerTop = isCover || contentTopTemp > headerViewHeight ? t: contentTopTemp - headerViewHeight;
 		// 下拉 header跟随效果
-		int headerBottom = contentTopTemp > headerViewHeight ? contentTopTemp
-				: headerViewHeight;
+		int headerBottom = isCover && contentTopTemp <= headerViewHeight ? headerViewHeight
+				: contentTopTemp;
 		mHeaderView.layout(l, headerTop, r, headerBottom);
-		mContentView.layout(l, contentTopTemp, r, getHeight());
+		mContentView.layout(l, contentTopTemp, r, b);
+//		resetContentHeight(contentTopTemp);
+//		resetHeaderHeight(contentTopTemp);
 	}
 
 	private void resetHeaderHeight(int contentTopTemp) {
@@ -176,8 +255,7 @@ public class SildingView extends RelativeLayout {
 
 	private void resetContentHeight(int contentTopTemp) {
 		if (mContentView != null && mContentView.getHeight() != 0) {
-			LayoutParams layoutParams = (LayoutParams) mContentView
-					.getLayoutParams();
+			LayoutParams layoutParams = (LayoutParams) mContentView.getLayoutParams();
 			layoutParams.height = getHeight() - contentTopTemp;
 			mContentView.setLayoutParams(layoutParams);
 		}
@@ -187,23 +265,22 @@ public class SildingView extends RelativeLayout {
 		new Handler().post(new Runnable() {
 			@Override
 			public void run() {
-				dragHelper.smoothSlideViewTo(mContentView, getPaddingLeft(),
-						top);
-				postInvalidate();
+				dragHelper.smoothSlideViewTo(mContentView, getPaddingLeft(),top);
+				computeScroll();
 			}
 		});
 	}
 
 	public void setHeaderView(int res) {
-		setHeaderView(LayoutInflater.from(getContext()).inflate(res, null));
+		setHeaderView(LayoutInflater.from(getContext()).inflate(res, this,
+				false));
 	}
 
 	public void setHeaderView(View view) {
 		if (mHeaderView != null)
 			this.removeView(mHeaderView);
 		mHeaderView = view;
-		addView(mHeaderView, new LayoutParams(LayoutParams.MATCH_PARENT,
-				LayoutParams.WRAP_CONTENT));
+		addView(mHeaderView);
 	}
 
 	public View getHeaderView() {
@@ -211,30 +288,30 @@ public class SildingView extends RelativeLayout {
 	}
 
 	public void setContentView(int res) {
-		setContentView(LayoutInflater.from(getContext()).inflate(res, null));
+		setContentView(LayoutInflater.from(getContext()).inflate(res, this,
+				false));
 	}
 
 	public void setContentView(View view) {
 		if (mContentView != null)
 			this.removeView(mContentView);
 		mContentView = view;
-		addView(mContentView, new LayoutParams(LayoutParams.MATCH_PARENT,
-				LayoutParams.MATCH_PARENT));
+		addView(mContentView);
 	}
 
 	public View getContentView() {
 		return mContentView;
 	}
 
-	public void setTopPosition(int position) {
-		this.topPosition = position;
+	public void setTopOffset(int position) {
+		this.topOffset = position;
 		if (contentViewtop < headerViewHeight) {
 			handleSlide(position);
 		}
 	}
 
-	public int getTopPosition() {
-		return topPosition;
+	public int getTopOffset() {
+		return topOffset;
 	}
 
 	public void setPullEnable(boolean isPullEnable) {
@@ -264,6 +341,10 @@ public class SildingView extends RelativeLayout {
 		if (contentViewtop < headerViewHeight) {
 			handleSlide(headerViewHeight);
 		}
+	}
+
+	public void setDragStart(boolean isDragStart) {
+		this.isDragStart = isDragStart;
 	}
 
 	public boolean isCover() {
